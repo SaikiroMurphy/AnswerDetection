040ce54522e8b0a134915a1bf973ae1c06cd21aa
SaikiroMurphy <ducanhn2810@gmail.com>
2024-07-18 11:56:01 +0700

    Fix bug

--- main.py
+++ main.py
@@ -142,4 +142,4 @@ async def predict(image: UploadFile):
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
-    uvicorn.run("main:app", host="127.0.0.1", port=6969, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=6969, reload=False)
